ce8a0154c52a9dbde06661a39356a1fe4a41b0d6
Amine Tayani <amine.tayanipro@gmail.com>
2024-02-17 12:59:10 +0100

    feat: Project completed

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -5,48 +5,40 @@
 	export let grid: string[];
 	export let found: string[];
 
-	let a = -1;
-	let b = -1;
-	let reset_timeout: number;
 	const dispatch = createEventDispatcher();
+
+	let a: number;
+	let b: number;
+	let reset_timeout: number;
 </script>
 
-<div class="grid">
-	{#each grid as emoji, i}
-		<Square
-			on:click={() => {
+{#each grid as square, i}
+	<Square
+		on:click={() => {
+			if (a > -1 && b > -1) {
 				clearTimeout(reset_timeout);
-				if (a === -1 && b === -1) {
-					a = i;
-				} else if (b === -1) {
-					b = i;
-					if (grid[a] === grid[b]) {
-						dispatch('found', {
-							emoji
-						});
-					} else {
-						reset_timeout = setTimeout(() => {
-							a = b = -1;
-						}, 1000);
-					}
-				} else {
-					b = -1;
-					a = i;
-				}
-			}}
-			{emoji}
-			selected={a === i || b === i}
-			found={found.includes(emoji)}
-		/>
-	{/each}
-</div>
+				a = i;
+				b = -1;
+			} else if (a > -1) {
+				b = i;
 
-<style>
-	.grid {
-		display: grid;
-		grid-template-columns: repeat(4, 1fr);
-		grid-template-rows: repeat(4, 1fr);
-		grid-gap: 0.5em;
-		height: 100%;
-	}
-</style>
+				if (grid[a] === grid[b]) {
+					// correct — remove from grid
+					found = [...found, grid[a]];
+					dispatch('found', { emoji: grid[a] });
+				} else {
+					// incorrect — reset after timeout
+					reset_timeout = setTimeout(() => {
+						a = b = -1;
+					}, 1000);
+				}
+			} else {
+				a = i;
+			}
+		}}
+		value={square}
+		selected={a === i || b === i}
+		found={found.includes(square)}
+		group={i === grid.indexOf(square) ? 'a' : 'b'}
+	/>
+{/each}
